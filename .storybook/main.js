5d07cea30202e2ba9f71817a5d241465d5f1eedf
Mouhamad THIAM <mohamed@ctsfares.com>
2021-05-05 18:04:45 +0000

    add JSX addon to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,9 @@
-// import {loadFontAwesome} from '../src/faLoader';
-
-// loadFontAwesome();
-
 module.exports = {
     stories: [
         '../src/components/**/*.stories.tsx',
         '../src/components/**/*.stories.jsx',
     ],
     addons: [
-        // 'storybook-addon-jsx/register'
+        'storybook-addon-jsx'
     ]
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,2 +1,8 @@
 import {loadFontAwesome} from '../src/faLoader';
+const { addDecorator } = require('@storybook/react');
+const { jsxDecorator } = require('storybook-addon-jsx');
+
+
+
 loadFontAwesome();
+addDecorator(jsxDecorator);
