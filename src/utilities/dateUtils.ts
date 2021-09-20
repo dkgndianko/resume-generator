df05fdeba2f3cc3de71850f5d2400eba752cb2ee
Mouhamad ND. THIAM <thiamouhamadpro@gmail.com>
2021-09-20 10:21:02 +0000

    add intl to fix dates when generating PDF

--- src/utilities/dateUtils.ts
+++ src/utilities/dateUtils.ts
@@ -1,3 +1,8 @@
-export const formatDate = (date: Date, format: Intl.DateTimeFormatOptions = {'year': 'numeric', 'month': 'long'}, locale: string = 'en-Us'): string => {
+import 'intl/locale-data/jsonp/en';
+const DEFAULT_FORMAT: Intl.DateTimeFormatOptions = {'year': 'numeric', 'month': 'long'};
+const DEFAULT_LOCALE: string = 'en-Us';
+
+export const formatDate = (date: Date, format: Intl.DateTimeFormatOptions = DEFAULT_FORMAT, locale: string = DEFAULT_LOCALE): string => {
+    // return Intl.DateTimeFormat(locale, format).format(date);
     return  date.toLocaleString(locale, format);
 }
